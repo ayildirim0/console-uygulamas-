e137b57e8104204230818c79548a2d2bde1e500b
bmade <bmade@MEHMET>
2025-06-16 14:04:54 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWord
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adem Yıldırım  patikanın uzman kadrosundan yararlanarak bende eksik ve kopuk olan noktaları tamamlamak için Bootcampyayım");
+
+        }
+    }
+}
